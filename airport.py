420bbedc4860735d5bb6064f2a3b8376ceef2229
mattyocode <matthewoliver@live.co.uk>
2020-07-19 21:49:49 +0100

    Chore: added pytest fixtures; added is_full method to airport

--- airport.py
+++ airport.py
@@ -1,6 +1,6 @@
 class Airport():
 
-    def __init__(self, id, capacity=5):
+    def __init__(self, id=1, capacity=5):
         self.id = id
         self.capacity = capacity
         self._terminals = []
@@ -8,3 +8,8 @@ class Airport():
     def plane_in_terminals(self, plane):
         return plane in self._terminals
 
+    def is_full(self):
+        return len(self._terminals) >= self.capacity
+
+    def empty_terminals(self):
+        self._terminals = []
--- tests/test_airport.py
+++ tests/test_airport.py
@@ -3,11 +3,21 @@ from plane import Plane
 import pytest
 from unittest.mock import Mock
 
-p = Mock()
+@pytest.fixture
+def airport():
+    airport = Airport()
+    yield airport
+    airport.empty_terminals
 
-def test_plane_not_at_airport_3():
-    a01 = Airport(1)
-    assert a01.plane_in_terminals(p) == False
+def test_plane_not_at_airport_3(airport):
+    assert airport.plane_in_terminals(1) == False
 
-def is_airport_full_4():
-    pass
+def test_plane_is_at_airport_4(airport):
+    print(airport._terminals)
+    airport._terminals = [1]
+    assert airport.plane_in_terminals(1) == True
+
+def test_is_airport_full_default_capacity(airport):
+    print(airport._terminals)
+    airport._terminals = [1, 2, 3, 4, 5]
+    assert airport.is_full() == True
