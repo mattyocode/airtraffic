b051cf516d6c7a9dad55ba2379201bfc58574a11
mattyocode <matthewoliver@live.co.uk>
2020-08-21 19:12:28 +0100

    Refactored weather to remove fixed state attribute

--- weather.py
+++ weather.py
@@ -3,25 +3,15 @@ import random
 class Weather:
     '''A class to return weather condition \
         10% rainy / 90 % sunny'''
-    
-    def __init__(self):
-        '''Initialises with no state'''
-        self.current_state = None
 
-    def set_state(self):
-        '''Returns random weather condition '''
-        n = random.randint(1, 10)
-        if n <=9:
-            self.current_state = 'Sunny'
-        else:
-            self.current_state = 'Stormy'
+    def generate_random_number(self):
+        return random.randint(1, 10)
 
     def check_state(self):
         '''Calls to set_state and returns \
             current state attribute'''
-        self.set_state()
-        return self.current_state
-
-    def clear(self):
-        '''Clears current state'''
-        self.current_state = None
+        n = self.generate_random_number()
+        if n <=9:
+            self.current_state = 'Sunny'
+        else:
+            self.current_state = 'Stormy'
